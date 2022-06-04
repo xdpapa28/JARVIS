86d1512f2071a06b5768283069d5865038984dd4
xdpapa28 <94953599+xdpapa28@users.noreply.github.com>
2022-06-04 08:28:32 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+clear
 
+apt update
+
+apt upgrade
+
+apt install python
+
+apt install python2
+
+apt install ruby
+
+gem install lolcat
+
+apt install termux-api
+
+pip install requests
+
+pip2 install requests
+
+pip install colorama
+
+pip2 install colorama
 pkg install screenfetch -y
 pkg install termux-api
